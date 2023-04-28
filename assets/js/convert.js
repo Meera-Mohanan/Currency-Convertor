452c5ecd52cc8b94a2bee1c87007a9c0b15e84b0
anju <anjualfino@gmail.com>
2023-04-29 00:16:11 +0800

    adding textbox and dropdowns for convert

--- assets/js/convert.js
+++ assets/js/convert.js
@@ -1,8 +1,15 @@
 $(function () {
-    $(document).ready(function() {
-        $("#convert-tab").click(function() {
-          $("#convert-tab").addClass("is-active");
-          $("#charts-tab").removeClass("is-active");
+    $(document).ready(function () {
+        $("#convert-tab").click(function () {
+            $("#convert-tab").addClass("is-active");
+            $("#charts-tab").removeClass("is-active");
         });
-      });
     });
+
+  /*   const amountEl = document.getElementById('input-amount');
+
+    amountEl.addEventListener('input', () => {
+        amountEl.value = amountEl.value.replace(/[^0-9]/g, '');
+    }); */
+
+});
