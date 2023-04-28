bdab0b27318a14ed1fec30b8b49e3cef7d52675d
anju <anjualfino@gmail.com>
2023-04-28 15:37:16 +0800

    adding basic index file

---
+++ assets/js/chart.js
@@ -0,0 +1,8 @@
+$(function () {
+$(document).ready(function() {
+    $("#charts-tab").click(function() {
+      $("#charts-tab").addClass("is-active");
+      $("#convert-tab").removeClass("is-active");
+    });
+  });
+});
---
+++ assets/js/convert.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(document).ready(function() {
+        $("#convert-tab").click(function() {
+          $("#convert-tab").addClass("is-active");
+          $("#charts-tab").removeClass("is-active");
+        });
+      });
+    });
