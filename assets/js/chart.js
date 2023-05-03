80d4e1710b3820b579e35e52b44bdc9d51e4f2a8
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-05-03 23:47:46 +0800

    files added

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,11 +1,62 @@
 $(function () {
-$(document).ready(function() {
-    $("#charts-tab").click(function() {
-      $("#charts-tab").addClass("is-active");
-      $("#convert-tab").removeClass("is-active");
-    });
+
+  const CHARTS_TAB_FLAG = 'chartsTabActive';
+  $("#charts-tab").click(function () {
+    $("#charts-tab").addClass("is-active");
+    $("#convert-tab").removeClass("is-active");
+    $("#convert-section").addClass('is-hidden');
+    $("#chart-section").removeClass('is-hidden');
+
+    //to make same the selection in dropdowns(convert section and chart section)
+    let fromCountry = $("#dropdown1").val();
+    let toCountry = $("#dropdown2").val();
+    $("#dropdown3").val(fromCountry);
+    $("#dropdown4").val(toCountry);
+    $("#dropdown3").trigger('change');
+    $("#dropdown4").trigger('change');
+  });
+  
+  //display chart button click event
+  $("#chartbtn").click(function () {
+    let fromCountry = $("#dropdown3").val();
+    let toCountry = $("#dropdown4").val();
+    displaychart(fromCountry,toCountry);
+
   });
 
-  
+  //toggle arrow click event
+  $("#arrow-icon1").click(function () {
+    let fromCountry = $("#dropdown3").val();
+    let toCountry = $("#dropdown4").val();
+    $("#dropdown3").val(toCountry);
+    $("#dropdown4").val(fromCountry);
+    $("#dropdown3").trigger('change');
+    $("#dropdown4").trigger('change');
+  });
+
+  //call api and add chart
+  function displaychart(fromCountry,toCountry){
+    const start_date = "2022-09-01";
+    const currentDate = new Date().toISOString().slice(0, 10);
+    let myHeaders = new Headers();
+    myHeaders.append("apikey", "ReKdzFAIwiuiMvUgxXgvSlqztSlMlDUc");
+
+    let requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+        headers: myHeaders
+    };
+    fetch(`https://api.apilayer.com/exchangerates_data/timeseries?start_date=${start_date}&end_date=${currentDate}&base=${fromCountry}&symbols=${toCountry}`, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      
+      const data = Object.entries(result.rates).map(([date, rates]) => ({ time: date, value: rates[toCountry] }));
+      const chart = LightweightCharts.createChart($("#chart-section")[0], { width: 400, height: 300 });
+      const lineSeries = chart.addLineSeries();
+      lineSeries.setData([]);
+      lineSeries.setData(data);
+    })
+    .catch(error => console.log('error', error));
+}
 
 });
--- assets/js/convert.js
+++ assets/js/convert.js
@@ -1,15 +1,171 @@
 $(function () {
-    $(document).ready(function () {
-        $("#convert-tab").click(function () {
-            $("#convert-tab").addClass("is-active");
-            $("#charts-tab").removeClass("is-active");
-        });
+
+    //convert tab click event
+    $("#convert-tab").click(function () {
+        $("#convert-tab").addClass("is-active");
+        $("#charts-tab").removeClass("is-active");
+        $("#chart-section").addClass('is-hidden');
+        $("#convert-section").removeClass('is-hidden');
+    });
+   
+
+    //toggle arrow click event
+    $("#arrow-icon").click(function () {
+        let fromCountry = $("#dropdown1").val();
+        let toCountry = $("#dropdown2").val();
+        $("#dropdown1").val(toCountry);
+        $("#dropdown2").val(fromCountry);
+        $("#dropdown1").trigger('change');
+        $("#dropdown2").trigger('change');
     });
 
-  /*   const amountEl = document.getElementById('input-amount');
+    //convert button click event
+    $("#convertbtn").click(function () {
+        let amount = $("#amount-ta").val();
+        let fromCountry = $("#dropdown1").val();
+        let toCountry = $("#dropdown2").val();
+        if (isNaN(amount)) {
+            $("#text-error-value").html("<strong>Please enter a valid amount</strong>");
+            return;
+        }
+        else if (!amount) {
+            amount = 1;
+        }
+        getConvertdata(amount, fromCountry, toCountry);
+    });
 
-    amountEl.addEventListener('input', () => {
-        amountEl.value = amountEl.value.replace(/[^0-9]/g, '');
-    }); */
+    //function calls
+    displaySearches();
+    displayPopular();
+
+    //dispaly the popular conversions with the selected from country
+    function displayPopular() {
+        $('#menu-popular').empty();
+        let fromCountry = $("#dropdown1").val();
+        const tc = ["EUR", "USD", "GBP", "AUD", "INR", "NZD", "SEK", "CAD"];
+        tc.forEach((toCountry) => {
+            if (fromCountry == toCountry) {
+                return;
+            }
+            const link = $('<a>').attr('href', '#').click(() => {
+                $("#dropdown1").val(fromCountry);
+                $("#dropdown2").val(toCountry);
+                $("#dropdown1").trigger('change');
+                $("#dropdown2").trigger('change');
+                getConvertdata(1, fromCountry, toCountry);
+            }).text(`${fromCountry} to ${toCountry}`);
+            const listItem = $('<li>').append(link);
+            $('#menu-popular').prepend(listItem);
+        });
+    }
+
+    //fetching local storage and adding click event to each search element
+    function displaySearches() {
+        $("#recent-searches").removeClass('is-hidden');
+        const currencyList = JSON.parse(localStorage.getItem('currencyList')) || [];
+        $('#menu-recent-searches').empty();
+
+        currencyList.forEach(({ from, to }) => {
+            const link = $('<a>').attr('href', '#').click(() => {
+                // TODO: optimize??
+                $("#dropdown1").val(from);
+                $("#dropdown2").val(to);
+                $("#dropdown1").trigger('change');
+                $("#dropdown2").trigger('change');
+                getConvertdata(1, from, to);
+            }).text(`${from} to ${to}`);
+            const clearBtn = $('<button>').attr('type', 'button').addClass('delete is-hidden').click(() => {
+                removeLocal(from, to);
+                displaySearches();
+            }).text('Clear');
+            // Add mouseenter and mouseleave event listeners to show/hide the clear button
+            link.on('mouseenter', () => {
+                clearBtn.removeClass('is-hidden');
+            });
+            link.on('mouseleave', () => {
+                clearBtn.addClass('is-hidden');
+            });
+            const listItem = $('<li>').append(clearBtn, link);
+            $('#menu-recent-searches').prepend(listItem);
+        });
+    }
+
+    //bulma loader show and hide functions
+    function showLoader() {
+        $("#progress").removeClass('is-hidden');
+    }
+    function hideLoader() {
+        $("#progress").addClass('is-hidden');
+    }
+
+    //adding fromcountry and tocountry to local storage
+    function addLocal(fromCountry, toCountry) {
+        let currencyList = [];
+        const max_list = 7;
+        if (localStorage.getItem("currencyList")) {
+            currencyList = JSON.parse(localStorage.getItem("currencyList"));
+        }
+        // use currencyList.find() to find duplicates
+        if (currencyList.find(item => item.from === fromCountry && item.to === toCountry)) {
+            return;
+        }
+        currencyList.push({ from: fromCountry, to: toCountry });
+        if (currencyList.length > max_list) {
+            currencyList = currencyList.slice(-max_list);
+        }
+        localStorage.setItem("currencyList", JSON.stringify(currencyList));
+    }
+
+    //clear the selected item from local storage
+    function removeLocal(fromCountry, toCountry) {
+        let currencyList = JSON.parse(localStorage.getItem("currencyList")) || [];
+        const index = currencyList.findIndex(item => item.from === fromCountry && item.to === toCountry);
+        if (index !== -1) {
+            currencyList.splice(index, 1);
+            localStorage.setItem("currencyList", JSON.stringify(currencyList));
+        }
+    }
+
+    //main function: call api and display current rates.
+    function getConvertdata(amount, fromCountry, toCountry) {
+        $("#text-error-value").html("");
+        let myHeaders = new Headers();
+        myHeaders.append("apikey", "ReKdzFAIwiuiMvUgxXgvSlqztSlMlDUc");
+        let requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+            headers: myHeaders
+        };
+        const url = `https://api.apilayer.com/exchangerates_data/convert?to=${toCountry}&from=${fromCountry}&amount=${amount}`;
+        showLoader();
+        fetch(url, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                hideLoader();
+                const convertedAmount = result.result;
+                const rate = result.info.rate;
+                const currenttimestamp = result.info.timestamp;
+                const dateObj = new Date(currenttimestamp * 1000);
+                $("#outputarea").html("<br>" + "<strong>" + amount + " " + fromCountry + " = " + convertedAmount + " " + toCountry + "</strong>");
+                $("#outputarea").append("<br>" + " 1 " + fromCountry + " = " + rate + " " + toCountry);
+                $("#datehere").html("<br>" + " Last updated on " + dateObj.toUTCString());
+                addLocal(fromCountry, toCountry);
+                displaySearches();
+                displayPopular();
+            })
+            .catch(error => console.log('error', error));
+    }
+
+    // Add a media query to rotate the arrow icon
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+    function handleMediaQuery(event) {
+        if (event.matches) {
+            $("#arrow-icon").addClass('rotate');
+        } else {
+            $("#arrow-icon").removeClass('rotate');
+        }
+    }
+    mediaQuery.addListener(handleMediaQuery);
+    handleMediaQuery(mediaQuery);
 
 });
